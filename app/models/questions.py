4886a2cec2bbf52c9460396f539f5b45a9e210fe
sangkips <sangkipkoech@gmail.com>
2022-12-10 11:00:06 +0300

    added alembic and made initial alembic migration

--- app/models/questions.py
+++ app/models/questions.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Text
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import relationship
 from app.db.database import Base
 from .users import User
